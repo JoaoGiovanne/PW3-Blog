ef1696d8e829462112c2badec752f596dc38fecd
joaoEgiovanne <joaogiovanneetec@gmail.com>
2024-08-06 11:16:01 -0300

    subindo projeto

---
+++ PW3-Blog/src/main/java/br/com/etechoracio/blog/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package br.com.etechoracio.blog.repository;
+
+import br.com.etechoracio.blog.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+<<<<<<< HEAD
+
+=======
+>>>>>>> 0c5878a16f46aaf88a1fe52c04b16aed1e5329d5
+}
