487ed91c9736febcb533ab63eaeb2503dc491f10
Joao Giovanne <joaogiovanneetec@gmail.com>
2024-08-13 11:17:12 -0300

    aula 13/08

--- PW3-Blog/src/main/java/br/com/etechoracio/blog/controller/PostController.java
+++ PW3-Blog/src/main/java/br/com/etechoracio/blog/controller/PostController.java
@@ -1,27 +1,22 @@
 package br.com.etechoracio.blog.controller;
-
-<<<<<<< HEAD
 import br.com.etechoracio.blog.entity.Post;
 import br.com.etechoracio.blog.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.SpringVersion;
-=======
-import br.com.etechoracio.blog.repository.PostRepository;
-import br.com.etechoracio.blog.entity.Post;
-import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> 0c5878a16f46aaf88a1fe52c04b16aed1e5329d5
+
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
-<<<<<<< HEAD
+
     @Autowired //cria o objeto quando necessario
     private PostRepository repository; // o repository manipula o dado
     @GetMapping
@@ -30,23 +25,13 @@ public class PostController {
     }
 
     @GetMapping("/{id}")
-    public Optional<Post> buscarPorId(@PathVariable Long id){ // é uma variavel que vai vir do path, seria a lista de id
-        return repository.findById(id);
+    public ResponseEntity<Post> buscarPorId(@PathVariable Long id){ // é uma variavel que vai vir do path, seria a lista de id
+        var resposta = repository.findById(id);
+        if (resposta.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        else {
+            return ResponseEntity.ok(resposta.get());
+        }
     }
 }
-=======
-
-    @Autowired
-    private PostRepository repository;
-
-    @GetMapping
-    public List<Post> listarPosts(){
-        return repository.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public Optional<Post> buscarPorId(@PathVariable Long id){
-        return repository.findById(id);
-    }
-}
->>>>>>> 0c5878a16f46aaf88a1fe52c04b16aed1e5329d5
--- PW3-Blog/src/main/java/br/com/etechoracio/blog/repository/PostRepository.java
+++ PW3-Blog/src/main/java/br/com/etechoracio/blog/repository/PostRepository.java
@@ -5,8 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 0c5878a16f46aaf88a1fe52c04b16aed1e5329d5
 }
